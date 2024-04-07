0622215cf070af8e032012a36a3de5f151fb8b6d
Yash <yashkhonde222@gmail.com>
2024-04-07 17:25:24 +0530

    Restructured analyze.py method to use multiple keys at once

--- testutils/views.py
+++ testutils/views.py
@@ -24,8 +24,8 @@ def analyze(request):
     new_line_remover = request.POST.get("new_line_remover", "off")
     extra_space_remover = request.POST.get("extra_space_remover", "off")
     char_count = request.POST.get("char_count", "off")
-    print(djtext)
-    print(removepunc)
+    # print(djtext)
+    # print(removepunc)
 
     # CHeck which checkbox is on
     if removepunc == "on":
@@ -35,21 +35,25 @@ def analyze(request):
             if char not in punctuations_list:
                 analyzed = analyzed + char
         params = {"purpose" : "Removed Punctuations from Sentence", "analyzed_text" : analyzed}
-        return render(request, "analyze.html", params)
-    elif (full_caps == "on"):
+        # return render(request, "analyze.html", params)
+        djtext = analyzed
+    # elif (full_caps == "on"):
+    if (full_caps == "on"):
         analyzed = ""
         for char in djtext:
             analyzed = analyzed + char.upper()
         params = {"purpose": "Text in Upper Case", "analyzed_text": analyzed}
-        return render(request, "analyze.html", params)
-    elif (new_line_remover == "on"):
+        # return render(request, "analyze.html", params)
+        djtext = analyzed
+    if (new_line_remover == "on"):
         analyzed = ""
         for char in djtext :
             if char != "\n" and char != "\r":
                 analyzed = analyzed + char
         params = {"purpose": "Remove New Line", "analyzed_text": analyzed}
-        return render(request, "analyze.html", params)
-    elif (extra_space_remover == "on"):
+        # return render(request, "analyze.html", params)
+        djtext = analyzed
+    if (extra_space_remover == "on"):
         analyzed = ""
         for index, char in enumerate(djtext):
             if djtext[index] == " " and djtext[index+1] == " ":
@@ -57,8 +61,9 @@ def analyze(request):
             else:
                 analyzed = analyzed + char
         params = {"purpose": "Extra space removed from sentence", "analyzed_text": analyzed}
-        return render(request, "analyze.html", params)
-    elif (char_count == 'on'):
+        # return render(request, "analyze.html", params)
+        djtext = analyzed
+    if (char_count == 'on'):
         analyzed= 0
         count = 0
         for i in djtext:
@@ -66,9 +71,13 @@ def analyze(request):
                 count += 1
         analyzed = analyzed + count
         params = {"purpose": "Counted Characters are, ", "analyzed_text": analyzed}
-        return render(request, "analyze.html", params)
-    else:
-        return HttpResponse("Error")
+        # return render(request, "analyze.html", params)
+
+    if (removepunc != "on" and full_caps != "on" and new_line_remover != "on" and extra_space_remover != "on" and char_count != "on"):
+        return HttpResponse("Please select operation atleat select one")
+    # else:
+    #     return HttpResponse("Error")
+    return render(request, "analyze.html", params)
 
 def dashboard(request):
     return render(request, "dashboard.html")
